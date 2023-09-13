0293166e5deb6ec43426805cae160184005b9035
Dario Dumlijan <dario.dumlijan1@gmail.com>
2023-09-13 14:49:20 +0200

    Fix for matching assets with pages deploy URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,27 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import get from 'lodash/get';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./__tests__/setupTests.ts'],
-    coverage: {
-      reporter: ['text', 'html'],
-      exclude: [
-        'node_modules/',
-      ],
+export default ({ mode }) => {
+  // import.meta.env.SECRET available here with: process.env.SECRET
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  const base = get(process.env, 'DEPLOYMENT_REPO');
+
+  // https://vitejs.dev/config/
+  return defineConfig({
+    plugins: [react()],
+    base: base ? `/${base}/` : '/',
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['./__tests__/setupTests.ts'],
+      coverage: {
+        reporter: ['text', 'html'],
+        exclude: [
+          'node_modules/',
+        ],
+      },
     },
-  },
-})
+  });
+}
